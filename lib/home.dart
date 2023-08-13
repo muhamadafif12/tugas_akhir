351210b814a877c33f32a0f902ca3b322f594dec
muhamadafif12 <142100223+muhamadafif12@users.noreply.github.com>
2023-08-13 17:42:10 +0700

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -2,14 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:proyel_akhir/hotel_favorite.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
+class HomePage extends StatelessWidget {
+  const HomePage({ super.key });
 
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
